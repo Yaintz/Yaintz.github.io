d85db9b2cb4a341fa322ad98195f9cbdf3fbc0e4
Yaintz <c6014109@gmail.com>
2025-02-27 16:03:40 -0500

    Added all of site files
    I knew you would come look at this

---
+++ SCOBY.js
@@ -0,0 +1,12 @@
+function home() {
+	window.location.href="SCOBY.html";
+}
+function shows() {
+	window.location.href="Shows.html";
+}
+function music() {
+	window.location.href="Music.html";
+}
+function EPK() {
+	window.location.href="EPK.html";
+}
