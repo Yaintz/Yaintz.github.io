665748438a2702c80a908d7169a010459fb84453
Yaintz <c6014109@gmail.com>
2025-03-14 17:17:26 -0400

    Changed to set location nav bar

--- SCOBY.js
+++ SCOBY.js
@@ -10,11 +10,19 @@ function music() {
 function EPK() {
 	window.location.href="EPK.html";
 }
+
 function resizeAlert() {
 	let width = document.documentElement.clientWidth;
 	let height = document.documentElement.clientHeight;
-	if (width > height) {
+	if (width > height && isMobile()) {
 		alert("please rotate your screen!");
 	}
 }
+function isMobile() {
+  		return /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+	}
 window.addEventListener("resize", resizeAlert);
+var loadingScreen = document.querySelector(".loadingScreen");
+window.addEventListener('load', function() {
+	loadingScreen.style.display = 'none';
+})
